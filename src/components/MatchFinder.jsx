5397c1722f936b16cb0c692e51c02a2f22866f99
Felipe Melendez <felipemelendez2021@gmail.com>
2022-02-10 09:50:23 -0800

    work in progress

--- src/components/MatchFinder.jsx
+++ src/components/MatchFinder.jsx
@@ -1,3 +1,25 @@
+// //<LetterBox state={foo}></LetterBox>
+// //
+// //</LetterBoxs>
+
+
+// // states
+// //
+// - notReadyForInput
+// - start herenot- ReadyForInput  -
+// first unfilled row// - scoredGreen
+// - after scoring// - scoredGray - after scoring
+// // - scoredYellow 4
+
+// // notReadyForInput -> readyForInput -> {scoredGreen, scoredGray, scoredYellow}
+
+// // state
+// {"0-1": "q"}
+
+// // describe a state where user guessed "robot" as first word
+// // state {guessingRow: 1, rows: {0: [{guess: "R", score: "green"}, {guess: "O", score: "yellow"}], 1: [], 2: [], 3: [], 4: [], 5: []}} readyForInput
+// 0: ""a, 1: ""o", 1: "ot"//notReadyFoconst MatchFinder = ({rowIndex, inputState, setInputState}) => {
+// /notReadyFo
 const MatchFinder = ({rowIndex, inputState, setInputState}) => {
 
   const changeHandler = (e) => {
